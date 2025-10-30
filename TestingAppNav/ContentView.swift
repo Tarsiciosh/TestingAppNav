53b83853659355daa9d48e7048895e14f639e9aa
Tar <tarsiciosh@gmail>
2025-10-30 08:24:50 -0300

    Initial Commit

---
+++ TestingAppNav/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestingAppNav
+//
+//  Created by Tarsicio Spraggon Hernández on 30/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestingAppNav/TestingAppNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAppNavApp.swift
+//  TestingAppNav
+//
+//  Created by Tarsicio Spraggon Hernández on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestingAppNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
