59759783336aeac2484cf03706a9daed3a86036a
Tar <tarsiciosh@gmail>
2025-11-17 16:38:38 -0300

    intermediate state

--- TestingAppNav/PresentedView.swift
+++ TestingAppNav/PresentedView.swift
@@ -7,21 +7,49 @@ struct PresentedView: View {
     @State private var searchText = ""
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("title")
-                .ignoresSafeArea(.all)
-                .padding(.top, 40)
-                .padding(.horizontal, 20)
-            
-            Text("some")
-                .background(.red)
-                .padding(.horizontal, 20)
-            
-            Rectangle()
-                .background(.blue)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("some")
+                        .background(.red)
+                        .padding(.horizontal, 20)
+
+                    Rectangle()
+                        .background(.blue)
+                }
+            }
+            .navigationTitle("Title")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+
+            }
+            .onAppear {
+                configureNavigationBarAppearance()
+            }
         }
-        .background(.orange)
-        .presentationDetents([.height(150)])
+        //.background(.orange)
+        //.presentationDetents([.height(150)])
+    }
+
+    private func configureNavigationBarAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+
+        // Customize large title font
+        appearance.largeTitleTextAttributes = [
+            .font: UIFont.systemFont(ofSize: 34, weight: .bold),
+            .foregroundColor: UIColor.label
+        ]
+
+        // Customize inline (collapsed) title font
+        appearance.titleTextAttributes = [
+            .font: UIFont.systemFont(ofSize: 17, weight: .semibold),
+            .foregroundColor: UIColor.label
+        ]
+
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
     }
 }
 
